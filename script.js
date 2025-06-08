1075329f566d9872d94fb6b43aca02df9107830a
cfcMalky <73409821+cfcMalky@users.noreply.github.com>
2025-06-08 03:01:35 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// ... all your existing code, but REMOVE this block:
+// const logoLink = document.getElementById('homeLogoLink');
+// if (logoLink) {
+//   logoLink.onclick = (e) => {
+//     e.preventDefault();
+//     showHome();
+//     closeAllMegamenus();
+//     closeMobileMenu();
+//   };
+// }
